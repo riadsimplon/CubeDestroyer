f5a335c52bcf3d686065dab03f027adfc6065187
unknown <riadsimplon>
2023-01-10 11:26:49 +0100

    bullet point 3

--- Assets/Scripts/cube_spawner.cs
+++ Assets/Scripts/cube_spawner.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Rendering;
+using Unity.VisualScripting;
 
 public class cube_spawner : MonoBehaviour
 {
-
+    [SerializeField] private int limitSpawned;
     [SerializeField] private Transform objectToSpawned;
-    [SerializeField] private int spawnSpeed = 3;
-    [SerializeField] private Transform spawnedDistance;
-    [SerializeField] private int maxSpawned;
+    [SerializeField] private int spawnSpeed;
+    private int numberSpawned;
     private List<Transform> spawnList = new List<Transform>();
-    private float time = Time.deltaTime;
+    private float time = 0f;
+    private Transform spawnedObject;
 
 
     // Start is called before the first frame update
@@ -24,14 +25,22 @@ public class cube_spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       private float time = Time.deltaTime;
+ 
 
-        if(time = 1/spawnSpeed &&)
-        {vector 3 worldPosition = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.nearClip);
-        Instantiate(objectToSpawned);
+        time += Time.deltaTime;
+
+     if (time >= spawnSpeed && numberSpawned<limitSpawned)
+        {
+            Debug.Log(time);
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range (0f,Screen.width), Random.Range(0f,Screen.height), Random.Range(10f, 30f)));
+            spawnedObject = Instantiate(objectToSpawned);
+            spawnedObject.position = worldPosition;
+            numberSpawned++;
+            spawnList.Add(spawnedObject);
+            time = 0f;
         }
         
+  }
 
-
-    }
+    
 }
